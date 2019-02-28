8a802e003ef2b607e3a452c293e1bd9b6ac03aba
Michael Caristo <mcaristo@nexient.com>
2019-02-27 22:52:06 -0500

    install antD

--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -8,40 +8,80 @@
  * reloading is not a necessity for you then you can refactor it and remove
  * the linting exception.
  */
+import 'antd/dist/antd.css';
 
+import {
+  Layout, Menu, Breadcrumb, Icon,
+} from 'antd';
 import * as React from 'react';
-import { Container, Row, Col } from 'reactstrap';
-import TopBar from '../TopBar';
-import Menu from '../Menu';
-import Orders from '../Orders';
+
+// import styled from 'styled-components'
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
   public render() {
-    const menu = (
-      <Menu isOpen={true}/>
-    );
 
-    const content = (
-      <Orders/>
-    );
+    const { SubMenu } = Menu;
+    const { Header, Content, Sider } = Layout;
 
     return (
-      <Container>
-        <Row>
-          <Col xs={12} sm={12} md={12} lg={12}>
-            <TopBar isOpen={true}/>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={2} sm={2} md={2} lg={2}>
-            {menu}
-          </Col>
-          <Col>
-            {content}
-          </Col>
-        </Row>
-      </Container>
+      <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['2']}
+          style={{ lineHeight: '64px' }}
+        >
+          <Menu.Item key="1">nav 1</Menu.Item>
+          <Menu.Item key="2">nav 2</Menu.Item>
+          <Menu.Item key="3">nav 3</Menu.Item>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider width={200} style={{ background: '#fff' }}>
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{ height: '100%', borderRight: 0 }}
+          >
+            <SubMenu key="sub1" title={<span><Icon type="user" />subnav 1</span>}>
+              <Menu.Item key="1">option1</Menu.Item>
+              <Menu.Item key="2">option2</Menu.Item>
+              <Menu.Item key="3">option3</Menu.Item>
+              <Menu.Item key="4">option4</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub2" title={<span><Icon type="laptop" />subnav 2</span>}>
+              <Menu.Item key="5">option5</Menu.Item>
+              <Menu.Item key="6">option6</Menu.Item>
+              <Menu.Item key="7">option7</Menu.Item>
+              <Menu.Item key="8">option8</Menu.Item>
+            </SubMenu>
+            <SubMenu key="sub3" title={<span><Icon type="notification" />subnav 3</span>}>
+              <Menu.Item key="9">option9</Menu.Item>
+              <Menu.Item key="10">option10</Menu.Item>
+              <Menu.Item key="11">option11</Menu.Item>
+              <Menu.Item key="12">option12</Menu.Item>
+            </SubMenu>
+          </Menu>
+        </Sider>
+        <Layout style={{ padding: '0 24px 24px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content style={{
+            background: '#fff', padding: 24, margin: 0, minHeight: 280,
+          }}
+          >
+            Content
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
     );
   }
 }
