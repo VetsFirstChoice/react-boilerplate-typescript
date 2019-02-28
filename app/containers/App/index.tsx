1b035e9e21c8b7227eccdda9af7327857d19e747
MikeMan107 <mrhodas107@gmail.com>
2019-02-28 16:19:38 -0500

    Added Product Menu Item

--- app/containers/App/index.tsx
+++ app/containers/App/index.tsx
@@ -19,11 +19,12 @@ import {
 import HomePage from 'containers/HomePage/Loadable';
 import ScriptPage from 'containers/ScriptPage/Loadable';
 import OrderPage from 'containers/OrderPage/Loadable';
+import ProductPage from 'containers/ProductPage/Loadable';
 import ExceptionPage from 'containers/ExceptionPage/Loadable';
 import AutoshipPage from 'containers/AutoshipPage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 
-import GlobalStyle from '../../global-styles';
+// import GlobalStyle from '../../global-styles';
 import styled from 'styled-components';
 
 export default function App() {
@@ -37,56 +38,58 @@ export default function App() {
   const { Header, Content, Sider, Footer } = Layout;
 
   return (
-
     <div>
       <Layout>
         <Sider
           breakpoint="lg"
           theme="light"
           collapsedWidth="0"
-          onBreakpoint={(broken) => { console.log(broken); }}
-          onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+          onBreakpoint={(broken) => {
+            console.log(broken);
+          }}
+          onCollapse={(collapsed, type) => {
+            console.log(collapsed, type);
+          }}
         >
-          <Logo > <img src={require('../../images/covetrus_logo.png')} alt="Test" style={{ width: '150px' }} /></Logo>
+          <Logo><img src={require('../../images/covetrus_logo.png')} alt="Test" style={{ width: '150px' }}/></Logo>
           {/* Set default key to store value */}
           <Menu mode="inline" defaultSelectedKeys={['1']}>
-            <Menu.Item key="1"><Link to="/"><Icon type="dashboard" theme="twoTone" />Dashboard</Link></Menu.Item>
-            <Menu.Item key="2"><Link to="/scripts"><Icon type="edit" theme="twoTone" />Scripts</Link></Menu.Item>
-            <Menu.Item key="3"><Link to="/orders"><Icon type="credit-card" theme="twoTone" />Orders</Link></Menu.Item>
-            <Menu.Item key="4">
-              <Link to="/exceptions"><Icon type="warning" theme="twoTone" />Exceptions</Link>
+            <Menu.Item key="1"><Link to="/"><Icon type="dashboard" theme="twoTone"/>Dashboard</Link></Menu.Item>
+            <Menu.Item key="2"><Link to="/scripts"><Icon type="edit" theme="twoTone"/>Scripts</Link></Menu.Item>
+            <Menu.Item key="3"><Link to="/orders"><Icon type="credit-card" theme="twoTone"/>Orders</Link></Menu.Item>
+            <Menu.Item key="4"><Link to="/products"><Icon type="shop" theme="twoTone"/>Products</Link></Menu.Item>
+            <Menu.Item key="5">
+              <Link to="/exceptions"><Icon type="warning" theme="twoTone"/>Exceptions</Link>
             </Menu.Item>
-            <Menu.Item key="5"><Link to="/autoship"><Icon type="schedule" theme="twoTone" />AutoShip</Link></Menu.Item>
-
+            <Menu.Item key="6"><Link to="/autoship"><Icon type="schedule" theme="twoTone"/>Autoship</Link></Menu.Item>
           </Menu>
         </Sider>
         <Layout>
-          <Header style={{ background: '#fff', padding: 0 }} >
+          <Header style={{ background: '#fff', padding: 0 }}>
             <div style={{ float: 'right' }}>
-              <Button style={{ margin: '15px' }} type="primary" shape="circle" icon="setting" />
-              <Button style={{ margin: '15px' }} type="primary" shape="circle" icon="bell" />
-              <Avatar style={{ backgroundColor: '#87d068', margin: '15px' }} icon="user" />
+              <Button style={{ margin: '15px' }} type="primary" shape="circle" icon="setting"/>
+              <Button style={{ margin: '15px' }} type="primary" shape="circle" icon="bell"/>
+              <Avatar style={{ backgroundColor: '#87d068', margin: '15px' }} icon="user"/>
             </div>
           </Header>
           <Content style={{ margin: '24px 16px 0' }}>
             <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
               <Switch>
-                <Route exact path="/" component={HomePage} />
-                <Route exact path="/scripts" component={ScriptPage} />
-                <Route exact path="/orders" component={OrderPage} />
-                <Route exact path="/exceptions" component={ExceptionPage} />
-                <Route exact path="/autoship" component={AutoshipPage} />
-                <Route component={NotFoundPage} />
+                <Route exact path="/" component={HomePage}/>
+                <Route exact path="/scripts" component={ScriptPage}/>
+                <Route exact path="/orders" component={OrderPage}/>
+                <Route exact path="/products" component={ProductPage}/>
+                <Route exact path="/exceptions" component={ExceptionPage}/>
+                <Route exact path="/autoship" component={AutoshipPage}/>
+                <Route component={NotFoundPage}/>
               </Switch>
             </div>
           </Content>
           <Footer style={{ textAlign: 'center' }}>
             ©2019 Covetrus
-      </Footer>
+          </Footer>
         </Layout>
       </Layout>
-
-      <GlobalStyle />
     </div>
   );
 }
