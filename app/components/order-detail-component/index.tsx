8a17cf27260844aab8549d7bd265dda819121fb5
Michael Caristo <mcaristo@vetsfirstchoice.com>
2019-03-15 16:14:40 -0400

    change to order details

--- app/components/order-detail-component/index.tsx
+++ app/components/order-detail-component/index.tsx
@@ -6,7 +6,7 @@
  */
 
 // remove, use babel for antD css
-import { Steps, Row, Col, Card, List, Table, Icon, Layout, Button, PageHeader } from 'antd';
+import { Steps, Row, Col, Card, List, Table, Icon, Layout, Button, PageHeader, Tag } from 'antd';
 import 'antd/dist/antd.css';
 
 import * as React from 'react';
@@ -109,43 +109,49 @@ export default class OrderDetail extends React.PureComponent {
         <PageHeader
           onBack={() => console.log('back!')}
           title="Order # 1000 0000 1696 80"
-          subTitle="Shipped 01/09/2018"
+          subTitle={<Tag color="volcano">Rush Order</Tag>}
           extra={[
-            <Button key="3" type="primary" ghost>Cancel Order</Button>,
-            <Button key="2" type="primary" ghost>Fraud</Button>,
-            <Button key="1" type="primary" ghost>Flag Order</Button>,
-            <Button key="1" type="primary" ghost>Refund Order</Button>,
+            <Button key="3" type="primary">Cancel Order</Button>,
+            <Button key="2" type="primary">Fraud</Button>,
+            <Button key="1" type="primary">Flag Order</Button>,
+            <Button key="1" type="primary">Refund Order</Button>,
           ]}
-          style={{ margin: '0px' }}
-        />
-        <Row gutter={16} style={{ margin: '0px', padding: '15px', background: '#fff' }}>
-          <Steps current={2} status="error">
+          style={{ margin: '0px', padding: '16px', backgroundColor: '#f0f2f5' }}
+        >
+          <Row style={{ padding: '10px' }}>
+            <Col span={6}>
+              <Row>
+                Order ID: 2253921
+          </Row>
+              <Row>
+                Platform Type: Vets First Choice
+          </Row>
+            </Col>
+            <Col span={6}>
+              <Row>
+                Order Origin: Pet Owner
+          </Row>
+              <Row>
+                Order Method: Web
+          </Row>
+            </Col>
+            <Col span={6}>
+              <Row>
+                Submitted By: Client
+          </Row>
+              <Row>
+                Type: Patient Specific
+          </Row>
+            </Col>
+          </Row>
+          <Steps current={2} status="error" style={{ padding: '10px' }}>
             <Step title="Placed" description="This is a description" />
             <Step title="Authorized" description="This is a description" />
             <Step title="Processing" description="This is a description" />
             <Step title="Shipped" description="This is a description" />
           </Steps>
-        </Row>
+        </PageHeader>
         <Row gutter={16} align="top" style={{ marginTop: '20px' }}>
-          <Col className="gutter-row" span={6}>
-            <Card size="small" title={<h3><Icon type="shop" theme="twoTone" />&nbsp;Practice</h3>}
-              actions={[<div><Icon type="team" /> Assist</div>, <div><Icon type="eye" /> View</div>]}>
-              <List
-                size="small"
-                dataSource={practiceDetailData}
-                renderItem={item => (
-                  <List.Item>
-                    <Col span={8}>
-                      <h4>{item[0]}</h4>
-                    </Col>
-                    <Col span={14}>
-                      <p>{item[1]}</p>
-                    </Col>
-                  </List.Item>
-                )}
-              />
-            </Card>
-          </Col>
           <Col className="gutter-row" span={6}>
             <Card size="small" title={<h3><Icon type="idcard" theme="twoTone" />&nbsp;Client</h3>}
               actions={[<div><Icon type="team" /> Assist</div>, <div><Icon type="eye" /> View</div>]}
@@ -160,6 +166,25 @@ export default class OrderDetail extends React.PureComponent {
                     </Col>
                     <Col span={14}>
                       <p style={{ wordBreak: 'break-all' }}>{item[1]}</p>
+                    </Col>
+                  </List.Item>
+                )}
+              />
+            </Card>
+          </Col>
+          <Col className="gutter-row" span={6}>
+            <Card size="small" title={<h3><Icon type="shop" theme="twoTone" />&nbsp;Practice</h3>}
+              actions={[<div><Icon type="team" /> Assist</div>, <div><Icon type="eye" /> View</div>]}>
+              <List
+                size="small"
+                dataSource={practiceDetailData}
+                renderItem={item => (
+                  <List.Item>
+                    <Col span={8}>
+                      <h4>{item[0]}</h4>
+                    </Col>
+                    <Col span={14}>
+                      <p>{item[1]}</p>
                     </Col>
                   </List.Item>
                 )}
