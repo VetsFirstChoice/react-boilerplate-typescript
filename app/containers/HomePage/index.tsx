1d0218fae1030d24bfb1fccc175993d4b67f2358
anoveskey <anoveskey@nexient.com>
2019-04-15 15:45:45 -0400

    Similar to EVO-33, but using an "A" or "B" setup

--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -8,11 +8,22 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import { Checkbox } from 'antd';
 
 export default function HomePage() {
+  const [ABTestingState, setABTestingState] = React.useState(false);
+  const toggleABTestingState = () => {
+    setABTestingState(!ABTestingState);
+  };
   return (
     <div>
       <FormattedMessage {...messages.header}/>
+      <br />
+      <span>Choose: </span>
+      <Checkbox checked={ABTestingState} onChange={toggleABTestingState}>
+        <span style={{display: !ABTestingState ? 'inline-block' : 'none'}}> either A</span>
+        <span style={{display: ABTestingState ? 'inline-block' : 'none'}}> or B</span>
+      </Checkbox>
     </div>
   );
 }
