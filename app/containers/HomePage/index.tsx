805b927144c36a6919c46ecf3de24dc06362ed58
anoveskey <anoveskey@nexient.com>
2019-04-15 15:10:30 -0400

    Saving progress for ticket EVO-33 - added checkbox and conditional display logic

--- app/containers/HomePage/index.tsx
+++ app/containers/HomePage/index.tsx
@@ -8,11 +8,25 @@
 import * as React from 'react';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
+import { Checkbox } from 'antd';
 
 export default function HomePage() {
+  const [fsToggleState, setFsState]  = React.useState(false);
+  const doStuff = () => {
+    setFsState(!fsToggleState);
+    // console.log('fsToggleState value: ', fsToggleState);
+  };
+
   return (
     <div>
       <FormattedMessage {...messages.header}/>
+      <div>
+        <Checkbox
+          checked={fsToggleState}
+          onClick={doStuff}
+        >Feature switch <span style={{display: fsToggleState ? 'inline-block' : 'none'}}>enabled</span>
+        </Checkbox>
+      </div>
     </div>
   );
 }
